07aaec2bb539af46bae43eb42f6dd73513132687
Ayan Huseynova <ayaneh@code.edu.az>
2022-10-02 02:53:44 +0400

    services - implementations and interfaces r done

--- Final-Project/Services/Implementations/BranchService.cs
+++ Final-Project/Services/Implementations/BranchService.cs
@@ -1,10 +1,72 @@
 ﻿using System;
+using Final_Project.Data;
+using Final_Project.Models;
+using Final_Project.Services.Interfaces;
+
 namespace Final_Project.Services.Implementations
 {
-    public class BranchService
+    public class BranchService : IBankService<Branch>, IBranchService
     {
+        private Bank<Branch> bank;
         public BranchService()
         {
+            bank = new Bank<Branch>();
+        }
+
+        public void Create(Branch entity)
+        {
+            if (entity.SoftDelete == false)
+            {
+                bank.Datas.Add(entity);
+            }
+        }
+
+        public void Delete(string name, string address, int budget)
+        {
+            Branch branch = bank.Datas.Find(x => x.Name.ToLower().Trim() == name.ToLower().Trim());
+            branch.SoftDelete = true;
+            GetEverything();
+        }
+
+        public void Get(string input)
+        {
+            Branch branch = bank.Datas.Find(m => m.Name.Contains(input.ToLower().Trim()) || m.Address.Contains(input.ToLower().Trim()));
+            Console.WriteLine(branch.Name + " " + branch.Address);
+        }
+
+        public void GetEverything()
+        {
+            foreach (var branch in bank.Datas.FindAll(m => m.SoftDelete == false))
+            {
+                Console.WriteLine(branch.Name + " " + branch.Address);
+            }
+        }
+
+        public void Updarw(string name, int budget, string address)
+        {
+            Branch branch = bank.Datas.Find(x => x.Name.ToLower().Trim() == name.ToLower().Trim());
+            branch.Address = address;
+            branch.Budget = budget;
+        }
+
+        public void GetProfit(int budget)
+        {
+
+        }
+
+        public void HireEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TransferEmployee(string name)
+        {
+
+        }
+
+        public void TransferMoney()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Final-Project/Services/Implementations/EmployeeService.cs
+++ Final-Project/Services/Implementations/EmployeeService.cs
@@ -1,10 +1,65 @@
 ﻿using System;
+using Final_Project.Data;
+using Final_Project.Models;
+using Final_Project.Services.Interfaces;
+
 namespace Final_Project.Services.Implementations
 {
-    public class EmployeeService
+    public class EmployeeService : IBankService<Employee>, IEmployeeService
     {
+        private Bank<Employee> bank;
         public EmployeeService()
         {
+            bank = new Bank<Employee>();
+        }
+
+        public void Create(Employee entity)
+        {
+            if (entity.SoftDelete == false)
+            {
+                bank.Datas.Add(entity);
+            }
+        }
+
+        public void Delete(string name)
+        {
+            Employee employee = bank.Datas.Find(x => x.Name.ToLower().Trim() == name.ToLower().Trim());
+            employee.SoftDelete = true;
+            GetEverything();
+        }
+
+        public void Get(string input)
+        {
+            try
+            {
+                Employee employee = bank.Datas.Find(m => m.Name.Contains(input.ToLower().Trim()) || m.Surname.Contains(input.ToLower().Trim()) || m.Profession.Contains(input.ToLower().Trim()));
+
+                Console.WriteLine(employee.Name + " " + employee.Surname + " " + employee.Profession);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Employee is not found");
+            }
+        }
+
+        public void GetEverything()
+        {
+            foreach (Employee employee in bank.Datas.FindAll(m => m.SoftDelete == false))
+            {
+                Console.WriteLine(employee.Name + " " + employee.Surname);
+            }
+        }
+
+        public void Update(string name, int salary, string profession)
+        {
+            Employee employee = bank.Datas.Find(x => x.Name.ToLower().Trim() == name.ToLower().Trim());
+            employee.Salary = salary;
+            employee.Profession = profession;
+        }
+
+        internal static void Create(object entity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
